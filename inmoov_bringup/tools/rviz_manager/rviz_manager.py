9cc2ad42bd5d32b350e441035917b3bbe417403a
supertick <grog@myrobotlab.org>
2024-05-16 06:38:50 -0700

    updated

--- inmoov_bringup/tools/rviz_manager/rviz_manager.py
+++ inmoov_bringup/tools/rviz_manager/rviz_manager.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # licensed under BSD-3
 
+import rclpy
+from rclpy.node import Node
 
-import rospy
-
-from inmoov_msgs.msg import MotorStatus
-from inmoov_msgs.msg import MotorCommand
+from inmoov_msgs.msg import MotorStatus, MotorCommand
 from inmoov_msgs.srv import MotorParameter
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
@@ -20,20 +19,14 @@ from PyQt5.QtWidgets import QPushButton, QMessageBox
 from threading import Thread
 
 from python_qt_binding import loadUi
-from python_qt_binding import QtGui
 from python_qt_binding.QtWidgets import QWidget
 
-#hacky way to add include directory to sys path
+# Hacky way to add include directory to sys path
 sys.path.append(os.path.join(dirname(dirname(dirname(abspath(__file__)))),'include'))
 
 from constants import PROTOCOL
 from servos import Servo
 from load_config_from_param import load_config_from_param
-
-servos = {}     # servo configuration data for robot
-joints = {}     # dict of joint names and position values
-
-bus = {}        # dict of motorcommand busses indexed by ordinal
 
 PI = 3.1415926539
 
@@ -44,57 +37,57 @@ PI = 3.1415926539
 gui = os.path.join(os.path.dirname(__file__), 'rviz_manager.ui')
 form_class = uic.loadUiType(gui)[0]
 
-def commandDispatcher(data):
+class RVizManager(Node):
+    def __init__(self):
+        super().__init__('rviz_command_dispatcher')
 
-    # if robot set up with degrees, then convert to radians
-    if rospy.get_param('/bringup/angles') == 'degrees':
-        p = list(data.position)
-        for x in range(0, len(p)):
-            p[x] = PI * p[x] / 180.0
+        # Subscribers
+        self.subscriber1 = self.create_subscription(
+            JointState,
+            'joint_command',
+            self.command_dispatcher,
+            10
+        )
+        self.subscriber2 = self.create_subscription(
+            JointState,
+            'joint_state',
+            self.state_dispatcher,
+            10
+        )
 
-        data.position = tuple(p)
+        # Publisher
+        self.publisher = self.create_publisher(JointState, 'rviz_command', 10)
 
-    publisher.publish(data)
+        load_config_from_param()
 
-def stateDispatcher(data):
+    def command_dispatcher(self, data):
+        if self.get_parameter('bringup.angles').get_parameter_value().string_value == 'degrees':
+            p = list(data.position)
+            for x in range(len(p)):
+                p[x] = PI * p[x] / 180.0
+            data.position = tuple(p)
+        self.publisher.publish(data)
 
-    # if robot set up with degrees, then convert to radians
-    if rospy.get_param('/bringup/angles') == 'degrees':
-        p = list(data.position)
-        for x in range(0, len(p)):
-            p[x] = PI * p[x] / 180.0
-
-        data.position = tuple(p)
-
-    publisher.publish(data)
-
-subscriber1 = rospy.Subscriber("joint_command", JointState, commandDispatcher)
-subscriber2 = rospy.Subscriber("joint_state", JointState, stateDispatcher)
-publisher = rospy.Publisher('rviz_command',JointState, queue_size=10)
-
-def init():
-
-    rospy.init_node('rviz_command_dispatcher', anonymous=False)
-    #rate = rospy.Rate(20) # 20hz
-
-    load_config_from_param()
-
-    #for j,b in rospy.get_param('/joints').items():
-    #    
-    #    number = rospy.get_param('/joints/' + j + '/bus')
-    #    busname = '/servobus/' + str(number).zfill(2) + '/motorcommand'
-
-    #    if not bus.has_key(number):
-    #        bus[number] = rospy.Publisher(busname, MotorCommand, queue_size=10)
-    #        rospy.loginfo('adding:  ' + busname)
+    def state_dispatcher(self, data):
+        if self.get_parameter('bringup.angles').get_parameter_value().string_value == 'degrees':
+            p = list(data.position)
+            for x in range(len(p)):
+                p[x] = PI * p[x] / 180.0
+            data.position = tuple(p)
+        self.publisher.publish(data)
 
 
-    rospy.spin()
-
+def main(args=None):
+    rclpy.init(args=args)
+    node = RVizManager()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
-    try:
-        init()
-    except rospy.ROSInterruptException:
-        pass
+    main()
