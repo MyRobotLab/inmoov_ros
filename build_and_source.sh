0fde85dbd1bff510399df411dafde86ae241f03d
supertick <grog@myrobotlab.org>
2024-05-16 21:59:46 -0700

    removed firmware

--- build_and_source.sh
+++ build_and_source.sh
@@ -2,6 +2,7 @@
 # build_and_source.sh
 
 cd ~/mrl/inmoov_ros
-colcon build --packages-select inmoov_bringup
+colcon build --packages-select inmoov_description
 source install/setup.bash
-ros2 launch inmoov_bringup bringup.launch.py
+# ros2 launch inmoov_bringup bringup.launch.py
+ros2 launch inmoov_description display.launch.py
